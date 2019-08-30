17fc9110b8c403c4acd778828d86c38f19535150
KazakovaNet <kazakovaNet@yandex.ru>
2019-08-30 15:02:25 +0300

    Add application coordinator

---
+++ KanjiApp/Coordinator/ApplicationCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  ApplicationCoordinator.swift
+//  KanjiApp
+//
+//  Created by Natalia Kazakova on 30/08/2019.
+//  Copyright © 2019 Natalia Kazakova. All rights reserved.
+//
+
+import Foundation

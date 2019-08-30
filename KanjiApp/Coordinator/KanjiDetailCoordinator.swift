542429943b68bc77e0f128d5a21becc298ad7e6b
KazakovaNet <kazakovaNet@yandex.ru>
2019-08-30 15:40:27 +0300

    Add kanji detail coordinator

---
+++ KanjiApp/Coordinator/KanjiDetailCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  KanjiDetailCoordinator.swift
+//  KanjiApp
+//
+//  Created by Natalia Kazakova on 30/08/2019.
+//  Copyright © 2019 Natalia Kazakova. All rights reserved.
+//
+
+import Foundation

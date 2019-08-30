99c26cd52ac020f39bfb15e9d31e5ab80260f2ab
KazakovaNet <kazakovaNet@yandex.ru>
2019-08-30 15:20:22 +0300

    Add kanji list coordinator

---
+++ AllKanjiListCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  AllKanjiListCoordinator.swift
+//  KanjiApp
+//
+//  Created by Natalia Kazakova on 30/08/2019.
+//  Copyright © 2019 Natalia Kazakova. All rights reserved.
+//
+
+import Foundation
